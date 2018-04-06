d23b33ce0f57879b942831069752c1bbac3ca2d5
LettoLt <Letto-l@yandex.ru>
2018-04-06 23:50:43 +0300

    init EF

---
+++ Data/ShopContext.cs
@@ -0,0 +1,12 @@
+﻿using Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data
+{
+    public class ShopContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public ShopContext(DbContextOptions<ShopContext> options) : base(options) { }
+    }
+}
